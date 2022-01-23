85f52ec7f593321d12e531e7a596909e5bc33ebf
untamedspud <1844347+untamedspud@users.noreply.github.com>
2022-01-23 16:59:36 -0500

    Update lab3_main.c

--- lab3_main.c
+++ lab3_main.c
@@ -7,7 +7,6 @@ int main()
         double computed_net_salary = net_salary( gross_salary, parking_cost, gym_membership_cost );
 
         printf( "The net salary is $%0.2lf.\n", computed_net_salary );
-
         puts( "Done." );
 
         return EXIT_SUCCESS;
