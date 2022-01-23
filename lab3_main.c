b00878f98edd120e1abf22c07facdd82dfba3b02
untamedspud <1844347+untamedspud@users.noreply.github.com>
2022-01-23 16:47:24 -0500

    Create lab3_main.c
    Basic main( ) function lab 3 spring 2022

---
+++ lab3_main.c
@@ -0,0 +1,12 @@
+int main()
+{
+        double gross_salary = prompt_for_gross( );
+        double parking_cost = prompt_for_parking( );
+        const double gym_membership_cost = 22.50;
+
+        double computed_net_salary = net_salary( gross_salary, parking_cost, gym_membership_cost );
+
+        printf( "The net salary is $%0.2lf.\n", computed_net_salary );
+
+        return EXIT_SUCCESS;
+}
